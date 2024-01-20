770c6faf2bc44a490b12d6947172a03836288637
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2024-01-20 11:45:06 +0530

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -31,14 +31,14 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  key_name               = "my key"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
   tags = {
-    Name = "Jenkins-sonar"
+    Name = "amazon clone"
   }
   root_block_device {
     volume_size = 30
