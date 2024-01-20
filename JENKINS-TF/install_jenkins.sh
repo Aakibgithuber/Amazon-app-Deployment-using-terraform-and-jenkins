a6d90da56e39956f64160800d103515fb4595718
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2024-01-20 20:19:07 +0530

    Update install_jenkins.sh

--- JENKINS-TF/install_jenkins.sh
+++ JENKINS-TF/install_jenkins.sh
@@ -15,9 +15,11 @@ sudo systemctl status jenkins
 #install docker
 sudo apt-get update
 sudo apt-get install docker.io -y
-sudo usermod -aG docker ubuntu  
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+sudo systemctl restart jenkins
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
 # install trivy
